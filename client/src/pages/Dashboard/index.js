cd108106161fb83df84c2194dd166a9007c9be9b
dipronil <dipronil.das@techwens.in>
2024-10-31 14:40:03 +0530

    delete

--- client/src/pages/Dashboard/index.js
+++
@@ -1,8 +0,0 @@
-import Employee from "./Employee";
-import Alljobs from "./Alljobs";
-import Profile from "./Profile";
-import SharedLayout from "./SharedLayout";
-import Stats from "./Stats";
-import Admin from "./Admin";
-
-export { Employee, Alljobs, Profile, SharedLayout, Stats, Admin };
--- client/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-
-import Login from "./Login";
-import Error from "./Error";
-import ProtectedRoute from "./ProtectedRoute";
-
-export { Login, Error, ProtectedRoute };
